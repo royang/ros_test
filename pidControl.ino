cfed21f112525a388fe0a2ee8f9ba7a6627c44fc
Roy Zhang <roydonghao@qq.com>
2018-08-13 21:59:02 +0800

    1.Vector3 instead Twist to report.2.added subscriber to subscribe pid value.3.added subscriber to subscribe new set point value

--- pidControl.ino
+++ pidControl.ino
@@ -4,66 +4,97 @@
 #include "ros.h"
 #include "rosserial_arduino/Adc.h"
 #include "std_msgs/String.h"
+#include "geometry_msgs/Vector3.h"
+#include "string.h"
 
 #define PPR2 1024
+#define PPR  65535
+
+void messageCb(const geometry_msgs::Vector3 &ctlMsg);
+void setPointCb(const std_msgs::String &val);
 
 double Setpoint=30, Input , Output;
 uint16_t LastEncoder=0, CurrentEncoder=0;
 
-int PPR = 65535;
 HardwareTimer timer_3(3);
 HardwareTimer timer_2(2);
+
 PID myPID(&Input, &Output, &Setpoint, 5, 1, 0.1, DIRECT);
 
+geometry_msgs::Vector3 rqt_var;
+
 ros::NodeHandle nh;
-rosserial_arduino::Adc adc_msg;
-std_msgs::String info;
-ros::Publisher  pub("pid", &adc_msg);
-ros::Publisher chatter("info", &info);
+ros::Publisher pub("pid", &rqt_var);
 
-char buf[128] = {0};
+std_msgs::String setPointVal;
+ros::Subscriber<std_msgs::String> setPointSub("/setPntSub", &setPointCb);
 
-void messageCb(std_msgs::String& str){
+ros::Subscriber<geometry_msgs::Vector3> sub("/pidCtrl", &messageCb);
+
+char buf[64]="Arduino init ok";
+
+void setPointCb(const std_msgs::String &val){
+  Setpoint = atoi(val.data);
+  memset(buf, 0, sizeof(buf));
+
+  snprintf(buf, sizeof(buf), "new setPoint：%d", (int)Setpoint);
   
+  nh.loginfo(val.data);
+  nh.loginfo(buf);
+}
+
+void messageCb(const geometry_msgs::Vector3 &ctlMsg){
+  digitalWrite(32, !digitalRead(32));
+
+  memset(buf, 0, sizeof(buf));
+  snprintf(buf, sizeof(buf), "kp:%.2f ki:%.2f kd:%.2f", ctlMsg.x, ctlMsg.y, ctlMsg.z);
+  nh.loginfo(buf);
+  nh.spinOnce();
+
+  timer_2.pause();
+  myPID.SetTunings(ctlMsg.x, ctlMsg.y, ctlMsg.z);
+  timer_2.resume();
 }
 
 void setup() {
   nh.initNode();
   nh.advertise(pub);
-  nh.advertise(chatter);
-
-  Serial.begin(115200);
+  nh.subscribe(sub);
+  nh.subscribe(setPointSub);
   
   pinMode(32, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(2, PWM);
 
-//configure timer as encoder
-  timer_3.setMode(0, TIMER_ENCODER); //set mode, the channel is not used when in this mode. 
-  timer_3.pause(); //stop... 
-  timer_3.setPrescaleFactor(1); //normal for encoder to have the lowest or no prescaler. 
-  timer_3.setOverflow(PPR);    //use this to match the number of pulse per revolution of the encoder. Most industrial use 1024 single channel steps. 
-  timer_3.setCount(0);          //reset the counter. 
-  timer_3.setEdgeCounting(TIMER_SMCR_SMS_ENCODER3); //or TIMER_SMCR_SMS_ENCODER1 or TIMER_SMCR_SMS_ENCODER2. This uses both channels to count and ascertain direction. 
-  timer_3.resume();                 //start the encoder... 
+  timer_3.setMode(0, TIMER_ENCODER); 
+  timer_3.pause(); 
+  timer_3.setPrescaleFactor(1); 
+  timer_3.setOverflow(PPR);    
+  timer_3.setCount(0);         
+  timer_3.setEdgeCounting(TIMER_SMCR_SMS_ENCODER3); 
+  timer_3.resume();                
 
   timer_2.setMode(2, TIMER_PWM);
   timer_2.setPrescaleFactor(71);
   timer_2.setOverflow(PPR2);
   timer_2.setCount(0);
-  timer_2.resume();
+
 
   pwmWrite(2, 0); 
 
   myPID.SetOutputLimits(0, PPR2);
   myPID.SetMode(AUTOMATIC);
+
+  delay(1000);
+
+  timer_2.resume();
 }
-unsigned long interval=0; //variable for status updates... 
+
+unsigned long interval=0; 
 
 void loop() {
-
   if (millis() - interval >= 30) { 
-     interval = millis(); //update interval for user. 
+     interval = millis();
      
      CurrentEncoder = timer_3.getCount();
      if(CurrentEncoder < LastEncoder){
@@ -71,23 +102,21 @@ void loop() {
      }else{
         Input = CurrentEncoder - LastEncoder;
      }
-     if(Input > 100){
-       digitalWrite(32, !digitalRead(32));
-       snprintf(buf, sizeof(buf), "cur:%d last:%d Input:%d\n", CurrentEncoder, LastEncoder, Input);
-       info.data = buf;
-       chatter.publish(&info);
-       nh.spinOnce();
-     }
+    //  if(Input > 100){
+      //  digitalWrite(32, !digitalRead(32));
+      //  snprintf(buf, sizeof(buf), "cur:%d last:%d Input:%d\n", CurrentEncoder, LastEncoder, Input);
+      //  nh.loginfo(buf);
+    //  }
      LastEncoder = CurrentEncoder;
 
      myPID.Compute();
      
      pwmWrite(2, Output);
 
-    adc_msg.adc0 = (uint16_t)Input*30;
-    adc_msg.adc1 = (uint16_t)Output;
-    adc_msg.adc2 = (uint16_t)Setpoint*30;
-    pub.publish(&adc_msg);
+    rqt_var.x = Input*30;
+    rqt_var.y = Output;
+    rqt_var.z = Setpoint*30;
+    pub.publish(&rqt_var);
     nh.spinOnce();
   }
 }
